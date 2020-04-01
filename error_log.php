3f96683f48caa531f7a3567189b15508528ebe22
Kimberly Hogeveen <2103405@talnet.nl>
2020-04-01 12:39:20 +0200

    verbeter

--- error_log.php
+++ error_log.php
@@ -1,13 +1,14 @@
 <?php
-function countDown() {
+function countDown()
+{
     $i = 12; 
     try {
         throw new Exception('foo');
     } catch (Exception $e) {
-        if ($i == 0 && $i < 11){
+        if ($i == 0 && $i < 11) {
             return 'Er is iets goed gegaan';  
-        } else error_log("er is iets fout gegaan") ;
-       
+        } else{ error_log("er is iets fout gegaan", "error.log") ;
+        }
     } 
 }
 echo countDown();
