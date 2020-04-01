14e7b410a78e4e7a1a18e3669337974425ff60da
Kimberly Hogeveen <2103405@talnet.nl>
2020-04-01 11:48:54 +0200

    Uitwerking van de exercise

--- my_error.php
+++ my_error.php
@@ -1,9 +1,14 @@
 <?php
+
 function countDown() {
+    $i = 12; 
     try {
         throw new Exception('foo');
     } catch (Exception $e) {
-        return 'Er is iets fout gegaan';
+        if ($i == 0 && $i < 11){
+            return 'Er is iets goed gegaan';  
+        } else error_log("er is iets fout gegaan") ;
+       
     } 
 }
 
