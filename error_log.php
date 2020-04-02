c82f76e2a17ec88f6edb1fdfc0bbaf48464fa573
Kimberly Hogeveen <2103405@talnet.nl>
2020-04-02 10:05:28 +0200

    verbeter blast

--- error_log.php
+++ error_log.php
@@ -1,3 +1,12 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
 function countDown()
 {
@@ -7,9 +16,16 @@ function countDown()
     } catch (Exception $e) {
         if ($i == 0 && $i < 11) {
             return 'Er is iets goed gegaan';  
-        } else{ error_log("er is iets fout gegaan", "error.log") ;
+        } else {
+            $error_message = "er is iets fout gegaan!"; 
+            $log_file = "./errors.log"; 
+            ini_set("log_errors", TRUE);  
+            ini_set('error_log', $log_file); 
+            error_log($error_message, 3, $log_file); 
         }
     } 
 }
 echo countDown();
 ?>
+</body>
+</html>
